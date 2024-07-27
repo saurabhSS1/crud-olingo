3bd96f99305368215ab71d12e2929eec412b0aa4
saurabhSS1 <suratkarsaurabh@gmail.com>
2024-07-27 18:37:16 +0530

    first commit

---
+++ src/test/java/com/example/odatav4/OdataV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odatav4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdataV4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
